5b391fd270022c35d46260af44f2eda03cda21ca
mohimakhatun <mohimakhatunkatwa@gmail.com>
2025-01-10 20:27:42 +0530

    mohimasmpk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from './components/components/login/LoginPage';
-import Register from './components/components/Auth/Register';
+// import LoginPage from './components/components/login/LoginPage';
+// import Register from './components/components/Auth/Register';
 import HomePage from './components/Header/Header';
 import PensionCalculator from './components/components/PensionComponent/PensionComponent';
 
@@ -10,14 +10,16 @@ function App() {
 
     return (
         <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/homepage" element={<HomePage/>}/>
-                <Route path="/pensionpage" element={<PensionCalculator/>}/>
-                {/* Add more routes as needed */}
-            </Routes>
-        </Router>
+        <Routes>
+            {/* <Route path='/' element={<LoginPage/>}/>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<Register />} /> */}
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/pensionpage" element={<PensionCalculator/>}/>
+            {/* Add more routes as needed */}
+        </Routes>
+    </Router>
+    
     )
 }
 
--- src/components/components/LowerNavbar/LowerNavbar.tsx
+++ src/components/components/LowerNavbar/LowerNavbar.tsx
@@ -24,22 +24,64 @@ const LowerNavbar: React.FC = () => {
                 </li>
                 <li className="menu-item dropdown">
                     <a href="#">Rules And Regulations</a>
-                    <ul className="dropdown-menu">
-                        <li><a href="#">Rule 1</a></li>
-                        <li><a href="#">Rule 2</a></li>
-                    </ul>
+                    
                 </li>
                 <li className="menu-item dropdown">
                     <a href="#">Port Info</a>
                     <ul className="dropdown-menu">
-                        <li><a href="#">Info 1</a></li>
-                        <li><a href="#">Info 2</a></li>
+                        <li><a href="#">Trade Notice </a></li>
+                        <li><a href="#">Sailent Features</a></li>
+                        <li><a href="#">Draft Forecast</a></li>
+                        <li><a href="#">Grievance</a></li>
+                        <li><a href="#">Tidal Notice</a></li>
+                        <li><a href="#">Maps</a></li>
+                        <li><a href="#">Environmental Information</a></li>
+                        <li><a href="#">Plans</a></li>
+                        <li><a href="#">Permission for Construction</a></li>
                     </ul>
                 </li>
                 <li className="menu-item dropdown">
+                    <a href="#">Media & Events</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Press Release</a></li>
+                        
+                    </ul>
+                </li>
+                <li className="menu-item dropdown">
+                    <a href="#">Job Opening</a>
+                    {/* <ul className="dropdown-menu">
+                        <li><a href="#">Info 1</a></li>
+                        <li><a href="#">Info 2</a></li>
+                    </ul> */}
+                </li>
+                <li className="menu-item dropdown">
+                    <a href="#">Concerns & Care </a></li>
+                <li className="menu-item dropdown">
+                    <a href="#">Corner</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="#">GST Corner</a></li>
+                        <li><a href="#">Medical</a></li>
+                        <li><a href="#"onClick={handelOnPensionClick}>Employee Corner</a></li>
+
+                    </ul>
+                </li>
+                {/* <li className="menu-item dropdown">
                     <a href="#" onClick={handelOnPensionClick}>Pension</a>
                 </li>
-                {/* Add more dropdown menus as required */}
+                Add more dropdown menus as required */}
+                <li className="menu-item dropdown">
+                    <a href="#">Contact Us</a>
+                    <ul className="dropdown-menu">
+                        <li><a href="#">SMPK Telephone Directory</a></li>
+                        <li><a href="#">SMPK Helpline</a></li>
+                        <li><a href="#">Office Details</a></li>
+                        <li><a href="#">Ministry of Shipping</a></li>
+                        <li><a href="#">Board of Trustees</a></li>
+                        <li><a href="#">Head of Dep. / Div.</a></li>
+                        <li><a href="#">Corporate Contact Details</a></li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     )
